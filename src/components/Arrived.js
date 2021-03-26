699eaba7930f4ee18d69c42fefc686f63eb3cee8
helen-huangg <huangg.helen@gmail.com>
2021-03-26 12:44:00 +0700

    cache image dan json dari rest api2

--- src/components/Arrived.js
+++ src/components/Arrived.js
@@ -6,7 +6,7 @@ function Arrived({items}) {
       <div className="container mx-auto mb-4">
         <div className="flex justify-center text-center mb-4">
           <h3 className="text-2xl capitalize font-semibold">
-            Just Arrived <br className="" />this summer for you
+            Just Arrived <br />this summer for you
           </h3>
         </div>
       </div>
